c89f4f80aaa3b9df5a93c9357672bfcc8aff5382
Luiz Paulo <lindennerd@gmail.com>
2023-01-24 22:51:28 -0300

    new: address value object class

---
+++ backend/DeliveryExpress.Domain/Common/Address.cs
@@ -0,0 +1,12 @@
+using DeliveryExpress.Domain.SeedWork;
+
+namespace DeliveryExpress.Domain.Common
+{
+    public class Addresss : ValueObject
+    {
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
