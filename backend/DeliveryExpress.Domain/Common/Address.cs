722fae92c8e0adad531891b059a2f3b2af91692a
Luiz Paulo <lindennerd@gmail.com>
2023-01-24 23:28:59 -0300

    feat: add Address Value Object

--- backend/DeliveryExpress.Domain/Common/Address.cs
+++ backend/DeliveryExpress.Domain/Common/Address.cs
@@ -2,11 +2,36 @@ using DeliveryExpress.Domain.SeedWork;
 
 namespace DeliveryExpress.Domain.Common
 {
-    public class Addresss : ValueObject
+    public class Address : ValueObject
     {
+        public Address(string street, string city, string country, string zipCode, string complement, string neighborhood)
+        {
+            Street = street;
+            City = city;
+            Country = country;
+            ZipCode = zipCode;
+            Complement = complement;
+            Neighborhood = neighborhood;
+        }
+
+        public string Street { get; } = null!;
+        public string City { get; } = null!;
+        public string State { get; } = null!;
+        public string Country { get; } = null!;
+        public string ZipCode { get; } = null!;
+        public int Number { get; }
+        public string Complement { get; } = null!;
+        public string Neighborhood { get; } = null!;
+
         protected override IEnumerable<object> GetEqualityComponents()
         {
-            throw new NotImplementedException();
+            yield return Street;
+            yield return State;
+            yield return Country;
+            yield return ZipCode;
+            yield return Number;
+            yield return Complement;
+            yield return Neighborhood;
         }
     }
 }
