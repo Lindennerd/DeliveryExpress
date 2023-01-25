be80fa50d9e85d0cf905433f69eb0cbc7445dadc
Luiz Paulo <lindennerd@gmail.com>
2023-01-25 00:10:38 -0300

    feat fluent validation for contact

--- backend/DeliveryExpress.Domain/StablishmentAggregator/Contact.cs
+++ backend/DeliveryExpress.Domain/StablishmentAggregator/Contact.cs
@@ -1,18 +1,33 @@
 using DeliveryExpress.Domain.SeedWork;
+using FluentValidation;
 
 namespace DeliveryExpress.Domain.StablishmentAggregator
 {
     public class Contact : Entity
     {
+        private readonly ContactValidator validator = new();
+
         public string Phone { get; } = null!;
         public string? Email { get; }
         public string Name { get; } = null!;
 
         public Contact(string name, string phone, string? email)
         {
+            validator.ValidateAndThrow(this);
+
             Name = name;
             Phone = phone;
             Email = email;
+        }
+    }
+
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            _ = RuleFor(c => c.Name).NotEmpty();
+            _ = RuleFor(c => c.Phone).NotEmpty();
+            _ = RuleFor(c => c.Email).EmailAddress();
         }
     }
 }
